b1e9a51cfa1aa7d7e1fdf91f15f2865826e5a9a9
wangyi <2698755786@qq.com>
2023-12-28 21:35:11 +0800

    修改了两个类属性

--- FS.h
+++ FS.h
@@ -1,13 +1,13 @@
 #ifndef FILESYS_FS_H
 #define FILESYS_FS_H
 #include <iostream>
-#include <array> //用来存储定量字符，模拟磁盘容量
 #include <vector>
 #include <string>
 #include <map>
 #define INODE_NUM 20
 #define I_BMAP_NUM 20
 #define D_BMAP_NUM 20
+#define DATA_BLOCK_NUM 512
 using namespace std;
 enum BlockType { INODE_BLOCK, FILE_BLOCK, INDIRECT_BLOCK, INDEX_BLOCK };//定义枚举类型，inode、文件、间接块儿、索引块儿
 
@@ -23,16 +23,17 @@ struct Disk_Block{
     int block_id;//这一块儿属于哪一个文件
     int block_size;
     union {
-        array<char,256> content;//存储文件的内容
+        char content[256];//存储文件的内容
         //存储间接块儿，可选择合适的数据结构
-        array<Disk_Block*,64>indirect_block;
+        Disk_Block* indirect_block[64];
         //存储索引块儿，可选择合适的数据结构
-        array<Dir_Index,10>index;
+        Dir_Index index[10];
     };
 };//磁盘块儿
 
 struct Inode{
     int i_number;//文件的低级名称
+    bool recycled;//文件是否放入回收站
     bool file_type;//文件类型
     int file_size;//文件大小
     string file_name;//文件名
@@ -40,42 +41,52 @@ struct Inode{
     int file_block;//分配了多少块儿
     vector<Disk_Block*>disk_pointer;//磁盘指针，会有指向间接块儿的指针
 };//inode节点
-
-class Disk {
-public:
-    Disk(int blockCount, int blockSize);
-    ~Disk();
-
-    Disk_Block* allocateBlock();//传入新建文件的i_number,file_type;并创建对应的inode和属性
-    Disk_Block* deleteBlock();
-    void displayDiskStatus();
-
-private:
-    const int blockCount;  // 磁盘块数量（常量，不可修改）
-    const int blockSize;   // 每个磁盘块的大小（常量，不可修改）
-    bool i_bitmap[I_BMAP_NUM];
-    bool d_bitmap[D_BMAP_NUM];//位示图
-    array<Disk_Block, 100> data_blocks;  // 固定大小的数组，模拟磁盘块
-    array<Inode,INODE_NUM> inodes_blocks;//存储inodes
-};//磁盘管理模块儿
+class Disk;
 
 class FileManagement{
 private:
     /*要有一个变量存放当前目录，进入子目录、返回上层目录时需要对应改变
      * 比如可以用当前的目录的inode节点
      */
-     Inode* ope_ptr;//工作指针
+    Inode* ope_ptr;//工作指针
+    Disk* disk;//磁盘指针
 public:
     /*
      * 对文件,目录的一些操作，主要是更改inode属性
      * 新建，修改文件的时候需要对应修改inode属性
      */
     FileManagement(Inode*root);//构造函数
-    void add_index(string file_name,int i_number);
-    void delete_index(string file_name,int i_number);
+    bool delete_index(string file_name,int i_number);
+    bool add_index(int i_number, BlockType blockType, string file_name);
     string get_dir();//返回当前的目录名
+    bool recycle_file(int i_number);
 
 };
+
+class Disk {
+
+private:
+    const int blockCount;  // 磁盘块数量（常量，不可修改）
+    const int blockSize;   // 每个磁盘块的大小（常量，不可修改）
+    bool i_bitmap[I_BMAP_NUM];
+    bool d_bitmap[D_BMAP_NUM];//位示图
+    Disk_Block data_blocks[DATA_BLOCK_NUM];  // 固定大小的数组，模拟磁盘块
+    Inode inodes_blocks[INODE_NUM];//固定大小的数组，模拟存放inodes的磁盘块儿
+    int arr[20]={0};//回收站，如果哪个节点被暂时删除，就把它的inode中的recycled字段置为true，并把inode号放入回收站
+public:
+    Disk(int blockCount, int blockSize);
+    ~Disk();
+
+    int allocateBlock(int i_number,BlockType type,string file_name);//返回inode号
+    bool deleteBlock();
+    void displayDiskStatus();
+    bool modify_file_name(string new_name);//更改名称
+    bool modify_file_recycled();//把recycled改为true
+
+
+};//磁盘管理模块儿
+
+
 
 class User {
 private:
@@ -113,5 +124,6 @@ edit()--读取并可以编辑
 //User的这些操作必须经过FileManagement完成
      */
 };
+
 
 #endif //FILESYS_FS_H
--- management.cpp
+++ management.cpp
@@ -1,10 +1,13 @@
 #include "FS.h"
 FileManagement::FileManagement(Inode *root) :ope_ptr(root){};
-void FileManagement::add_index(string file_name, int i_number) {
-
+bool FileManagement::add_index(int i_number,BlockType blockType,string file_name) {
+    /*
+     * 新建文件夹、文件其实都是相当于让磁盘分配空间，因此直接调用磁盘的函数即可
+     */
+    disk->allocateBlock(i_number,blockType,file_name);
 }
-void FileManagement::delete_index(string file_name, int i_number) {
-
+bool FileManagement::recycle_file(int i_number)  {
+    disk->modify_file_recycled();
 }
 string FileManagement::get_dir() {
 
