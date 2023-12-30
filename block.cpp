8563c922439cb76958965907eb12ec0ad77e8287
wangyi <2698755786@qq.com>
2023-12-30 11:53:10 +0800

    更改了一些函数的返回值类型

--- block.cpp
+++ block.cpp
@@ -1,7 +1,27 @@
 #include "FS.h"
-string Disk::read_file(int i_number) {
+void Disk::read_file(int i_number) {
     /*
      * 知道具体分配策略才能够知道怎么读
      */
     //for(auto iter:)
+}
+void Disk::read_dir(int i_number) {
+    /*
+     * 将目录的信息仿佛缓冲区中
+     */
+}
+string Disk::modify_time() {
+    // 获取当前时间点
+    auto currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+    // 将时间点转换为本地时间结构
+    std::tm* localTime = std::localtime(&currentTime);
+
+    // 创建一个字符数组来存储时间字符串
+    const int bufferSize = 80;
+    char buffer[bufferSize];
+
+    // 使用 std::strftime 将时间结构格式化为字符串
+    std::strftime(buffer, bufferSize, "%Y/%m/%d %H:%M", localTime);
+    return buffer;//返回当前日期时间字符串
 }
