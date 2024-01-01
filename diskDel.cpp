589a1841d729cb846e4f24009212345beb052a49
GyxCCCC <486126146@qq.com>
2024-01-01 16:46:16 +0800

    修改删除磁盘块部分

--- diskDel.cpp
+++ diskDel.cpp
@@ -26,35 +26,130 @@ bool Disk::deleteBlock(int i_number) {
 		return false;
 	}
 
-	// 查找inode对应的数据块
-	vector<Disk_Block*>& disk_pointer = inodes_blocks[inode_id].disk_pointer;
-	for (Disk_Block* block : disk_pointer) {
-		int block_id = block->block_id;
+	// bool file_type;//文件类型 0-文件 1-目录
+	// 删除的文件类型为文件，需要删除直接块和间接块的内容
+	if (!inodes_blocks[inode_id].file_type) {
+		// 删除直接块
+		for (int i = 0; i < 12; i++) {
+			Disk_Block* block = inodes_blocks[inode_id].direct_block[i];
+			if (block == nullptr) {
+				break;
+			}
+			else if (block->blockType == FILE_BLOCK && block->block_id == i_number) {
+				// 找到该直接块在数据区域的位置
+				int block_id = -1;
+				for (int j = 0; j < DATA_BLOCK_NUM; j++) {
+					if (&data_blocks[j] == block) {
+						block_id = j;
+						break;
+					}
+				}
+				// 标记数据块为空闲
+				d_bitmap[block_id] = false;
+				// 清空数据块里的内容
+				memset(block->content, 0, sizeof(block->content));
+				// 恢复数据块其他信息为默认状态
+				block->occupied = false;
+				block->block_id = -1;
+				block->block_size = 0;
+				memset(block->content, 0, 256);
+				for (int j = 0; j < 32; j++)
+					block->indirect_block[j] = nullptr;
+				for (int j = 0; j < 10; j++)
+					block->index[j] = { 0 };
+				// 更新inode的磁盘指针
+				block = nullptr;
+			}
+		}
 
-		// 将查找到的数据块恢复默认数值并删除相应内容
-		data_blocks[block_id].occupied = false;
-		data_blocks[block_id].block_id = -1;
-		data_blocks[block_id].block_size = 0;
-		memset(data_blocks[block_id].content, 0, sizeof(Dir_Index) * 20);
+		// 删除间接块
+		for (int i = 0; i < 32; i++) {
+			Disk_Block* block = inodes_blocks[inode_id].disk_pointer[i];
+			if (block == nullptr) {
+				break;
+			}
+			else if (block->blockType == INDIRECT_BLOCK && block->block_id == i_number) {
+				// 找到在数据区域的位置
+				int block_id = -1;
+				for (int j = 0; j < DATA_BLOCK_NUM; j++) {
+					if (&data_blocks[j] == block) {
+						block_id = j;
+						break;
+					}
+				}
+				// 标记数据块为空闲
+				d_bitmap[block_id] = false;
+				// 清空数据块的内容
+				memset(block->content, 0, sizeof(block->content));
+				// 恢复数据块其他信息为默认状态
+				block->occupied = false;
+				block->block_id = -1;
+				block->block_size = 0;
+				memset(block->content, 0, 256);
+				for (int j = 0; j < 32; j++)
+					block->indirect_block[j] = nullptr;
+				for (int j = 0; j < 10; j++)
+					block->index[j] = { 0 };
+				// 更新间接块中的指针
+				block = nullptr;
+			}
+		}
+	}
 
-		d_bitmap[block_id] = false;
 
-		Inode& inode = inodes_blocks[inode_id];
-		auto it = find(inode.disk_pointer.begin(), inode.disk_pointer.end(), &data_blocks[block_id]);
-
-		if (it != inode.disk_pointer.end())
-			inode.disk_pointer.erase(it);
+	// 删除的文件类型为目录，需要删除直接块的内容
+	if (inodes_blocks[inode_id].file_type) {
+		// 删除直接块
+		for (int i = 0; ; i++) {
+			Disk_Block* block = inodes_blocks[inode_id].direct_block[i];
+			if (block == nullptr) {
+				break;
+			}
+			else if (block->blockType == FILE_BLOCK && block->block_id == i_number) {
+				// 找到该直接块在数据区域的位置
+				int block_id = -1;
+				for (int j = 0; j < DATA_BLOCK_NUM; j++) {
+					if (&data_blocks[j] == block) {
+						block_id = j;
+						break;
+					}
+				}
+				// 标记数据块为空闲
+				d_bitmap[block_id] = false;
+				// 清空数据块里的内容
+				memset(block->content, 0, sizeof(block->content));
+				// 恢复数据块其他信息为默认状态
+				block->occupied = false;
+				block->block_id = -1;
+				block->block_size = 0;
+				memset(block->content, 0, 256);
+				for (int j = 0; j < 32; j++)
+					block->indirect_block[j] = nullptr;
+				for (int j = 0; j < 10; j++)
+					block->index[j] = { 0 };
+				// 更新inode的磁盘指针
+				block = nullptr;
+			}
+		}
 	}
 
 	// 删除inode块中的内容
-	inodes_blocks[inode_id].i_number = i_number;
-	inodes_blocks[inode_id].recycled = true;
-	inodes_blocks[inode_id].file_type = 1;
-	inodes_blocks[inode_id].file_size = 0;
-	inodes_blocks[inode_id].file_name = "";
-	inodes_blocks[inode_id].file_block = 0;
+	for (int i = 0; i < INODE_NUM; i++) {
+		if (i_bitmap[i] && inodes_blocks[i].i_number == i_number) {
+			inodes_blocks[inode_id].i_number = -1;
+			inodes_blocks[inode_id].recycled = true;
+			inodes_blocks[inode_id].file_size = 0;
+			inodes_blocks[inode_id].file_name = "";
+			inodes_blocks[inode_id].file_block = 0;
+			for (int j = 0; j < 12; j++) {
+				inodes_blocks[i].direct_block[j] = nullptr;
+			}
+			inodes_blocks[i].disk_pointer.clear();
 
-	i_bitmap[inode_id] = false;
+			i_bitmap[inode_id] = false;
+		}
+	}
+
 
 	cout << "删除成功" << endl;
 	return true;
@@ -73,7 +168,6 @@ void Disk::displayDiskStatus() {
 				sizeof(inodes_blocks[i].file_size) +
 				sizeof(inodes_blocks[i].file_block) +
 				sizeof(inodes_blocks[i].file_name) +
-				sizeof(inodes_blocks[i].modified_time) +
 				sizeof(Disk_Block*) * inodes_blocks[i].disk_pointer.size();
 		}
 	}
@@ -84,7 +178,6 @@ void Disk::displayDiskStatus() {
 		// 找到一个非空闲的inode
 		if (d_bitmap[i]) {
 			data_memory_size = sizeof(data_blocks[i].occupied) +
-				sizeof(data_blocks[i].type) +
 				sizeof(data_blocks[i].blockType) +
 				sizeof(data_blocks[i].block_id) +
 				sizeof(data_blocks[i].block_size);
